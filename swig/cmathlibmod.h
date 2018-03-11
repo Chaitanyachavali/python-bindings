2308e1a149d9a5b816e40a069478fa44474b3c6e
Chaitanya Reddy <chavalichaithanyachinna@gmail.com>
2018-03-11 22:55:34 +0530

    adding c lib

---
+++ swig/cmathlibmod.h
@@ -0,0 +1,7 @@
+extern int add(int x, int y);
+extern int sub(int x, int y);
+extern int mul(int x, int y);
+extern int divide(int x, int y);
+extern int mod(int x, int y);
+extern int sumofn(int x);
+extern int factorial(int x);
